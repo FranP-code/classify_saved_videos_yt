fa5df4c16cba83e12464878acb79c64c4b6cec59
Emi <lunaemilio2003@gmail.com>
2025-07-05 22:47:37 -0300

    first commit to linux

--- script.py
+++ script.py
@@ -1,17 +1,35 @@
 import pyautogui as pgui
 import time
-import keyboard as kb
+from pynput import keyboard as kb
 
-# colocar el nombre de tu navegador, basado en el nombre de la imagen de su respectivo icono
-browser = 'brave'
 
-browser_loc = pgui.locateCenterOnScreen(f'img/{browser}.png', confidence=0.8)
+quit = False
+
+def on_press_to_quit(key):
+    """
+    Quit program if 'q' is pressed
+    """
+    global quit
+    try:
+        if key.char == 'q':
+            print('closing program')
+            quit = True
+    except AttributeError:
+        pass
+
+listener = kb.Listener(on_press=on_press_to_quit)
+listener.start()
+
+# Enter the name of your browser, based on the name of its icon image.
+browser_img = 'brave.png'
+
+browser_loc = pgui.locateCenterOnScreen(f'img/{browser_img}', confidence=0.8)
 
 pgui.moveTo(browser_loc, duration=0.2)
 time.sleep(0.2)
 pgui.click()
 
-# cambia el tiempo dependiendo de cuanto tarde en abrir tu navegador
+# Change the time depending on how long it takes to open your browser
 time.sleep(2)
 
 
@@ -22,27 +40,29 @@ pgui.write('https://www.youtube.com/playlist?list=WL')
 time.sleep(0.05)
 pgui.press("enter")
 
-# cambiar dependiendo de cuanto tarde en cargarte yt
+# change depending on how long it takes to load yt
 time.sleep(5)
 
-videos = int(input('number of videos: '))
 
-while videos != 0:
+if __name__ == '__main__':
+    while not quit:
 
-    if kb.is_pressed('q'):
-        print(':(')
-        break
-    
-    opt_loc = pgui.locateCenterOnScreen('img/options.png', confidence=0.8)
+        opt_loc = pgui.locateCenterOnScreen('img/options.png', confidence=0.8)
 
-    pgui.moveTo(opt_loc, duration=0.2)
-    pgui.click()
-    time.sleep(0.2)
+        if opt_loc:
+            pgui.moveTo(opt_loc, duration=0.2)
+            pgui.click()
+            time.sleep(0.2)
+        else: 
+            print('options button not founded')
+            break
 
-    del_loc = pgui.locateCenterOnScreen('img/delete.png', confidence=0.8)
+        del_loc = pgui.locateCenterOnScreen('img/delete.png', confidence=0.8)
 
-    pgui.moveTo(del_loc, duration=0.2)
-    pgui.click()
-    time.sleep(0.2)
-
-    videos -= 1
+        if del_loc:
+            pgui.moveTo(del_loc, duration=0.2)
+            pgui.click()
+            time.sleep(0.2)
+        else: 
+            print('delete button not founded')
+            break
